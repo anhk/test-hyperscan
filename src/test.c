bb4e106d5832121e2b07b11790ac540afcbd4613
anhk <anhk_@126.com>
2017-06-09 14:25:19 +0800

    first commit

---
+++ src/test.c
@@ -0,0 +1,93 @@
+
+
+#include "hs.h"
+
+#include <stdio.h>
+#include <string.h>
+
+static const char *patterns[] = {
+    "http://foo",
+    "http://.*bar",
+};
+
+char buff[] = "http://foobar";
+
+
+#define dd(fmt, ...) do {               \
+    fprintf(stderr, "[%s:%d] "fmt"\n",  \
+            __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+} while (0)
+
+
+
+/**
+ * HS_FLAG_CASELESS     :
+ * HS_FLAG_MULTILINE    :
+ * HS_FLAG_DOTALL       :
+ * HS_FLAG_SINGLEMATCH  :
+ * HS_FLAG_ALLOWEMPTY   :
+ * HS_FLAG_UTF8         :
+ * HS_FLAG_UCP          :
+ **/
+
+static const unsigned int flags[] = {
+    HS_FLAG_CASELESS,
+    HS_FLAG_CASELESS
+};
+
+
+static const unsigned int ids[] = {
+    10000,
+    10001,
+};
+
+static int onMatch(unsigned int id, unsigned long long from, unsigned long long to,
+        unsigned int flags, void *ctx)
+{
+    dd("id=%d, from=%llx, to=%llu, flags=%x, ctx=%p",
+        id, from, to, flags, ctx);
+    return 0; // continue matching
+}
+
+
+int main(int argc, char **argv)
+{
+    hs_database_t *db_block;
+    hs_compile_error_t *compileErr;
+    hs_error_t err;
+
+    hs_scratch_t *scratch;
+    size_t matchCount;
+
+    err = hs_compile_multi(patterns, flags, ids,
+            sizeof(patterns) / sizeof(patterns[0]),
+            HS_MODE_BLOCK, NULL, &db_block, &compileErr);
+
+    if (err != HS_SUCCESS) {
+        if (compileErr->expression < 0) {
+            dd("ERROR: %s", compileErr->message);
+        } else {
+            dd("ERROR: Pattern '%s' failed compilation with error: %s",
+                    patterns[compileErr->expression],
+                    compileErr->message);
+        }
+        hs_free_compile_error(compileErr);
+        exit(-1);
+    }
+
+    err = hs_alloc_scratch(db_block, &scratch);
+    if (err != HS_SUCCESS) {
+        dd("ERROR: could not allocate scratch space. Exiting.");
+        exit(-1);
+    }
+
+    err = hs_scan(db_block, buff, strlen(buff), 0, scratch, onMatch, &matchCount);
+    if (err != HS_SUCCESS) {
+        dd("ERROR: Unable to scan packet. Exiting.");
+        exit(-1);
+    }
+
+    return 0;
+}
+
+
